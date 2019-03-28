d24a2d2b8491e139b784d0daedde607a1055d1d4
Jeonghun Ban <bjh5230@gmail.com>
2019-03-28 18:47:22 +0900

    random 중복 err 수정

--- lotto/views.py
+++ lotto/views.py
@@ -6,17 +6,20 @@ def home(request):
     return render(request, 'home.html')
 
 def result(request):
-    # 입력한 값 받아오기
+    # input number
     input = list();
     for i in range(0,6):
         input.append(int(request.GET['number'+str(i+1)]))
 
-    # 추첨
+    # random number
     rand_num = list() 
     for i in range(0,7):
-        rand_num.append(random.randrange(1,47))
+        rand = random.randrange(1,46)
+        while rand in rand_num:
+            rand = random.randrange(1,46)
+        rand_num.append(rand)
 
-    # 맞춘 횟수
+    # count same number
     count=0
     for i in range(0,6):
         for j in range(0,7):
