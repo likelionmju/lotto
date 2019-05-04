0506f548618c5ee94e90ba5657f0f42338a1ad47
Jongwoo Han <han980817@gmail.com>
2019-05-05 02:15:16 +0900

    Heroku 배포 준비

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_mrh23w6(z7e3gep@u%#u*##n!ih(^$wbhk!9)ahrubq28e1on'
+# SECRET_KEY = '_mrh23w6(z7e3gep@u%#u*##n!ih(^$wbhk!9)ahrubq28e1on'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_mrh23w6(z7e3gep@u%#u*##n!ih(^$wbhk!9)ahrubq28e1on')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
