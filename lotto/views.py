2377702174275170204dba320045d7434901b216
Jeonghun <bjh5230@gmail.com>
2019-05-06 21:38:36 +0900

    modify variable name in views.py

--- lotto/views.py
+++ lotto/views.py
@@ -7,27 +7,28 @@ def home(request):
 
 def result(request):
     # input number
-    input = list();
+    number_list = list()
     for i in range(0,6):
         number = request.GET['number'+str(i+1)]
         # if number is null, redirect home.html
         if number == '':
             return redirect('home')
-        input.append(int(number))
+        number_list.append(int(number))
  
     # random number
-    rand_num = list() 
+    rand_list = list() 
     for i in range(0,7):
         rand = random.randrange(1,46)
-        while rand in rand_num:
+        # when rand in rand_list
+        while rand in rand_list:
             rand = random.randrange(1,46)
-        rand_num.append(rand)
+        rand_list.append(rand)
 
     # count same number
     count=0
     for i in range(0,6):
         for j in range(0,7):
-            if input[i]==rand_num[j]:
+            if number_list[i]==rand_list[j]:
                 count+=1
 
-    return render(request, 'result.html', {'rand_num':rand_num, 'input':input, 'count':count })
+    return render(request, 'result.html', {'number_list':number_list, 'rand_list':rand_list, 'count':count })
